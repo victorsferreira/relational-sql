bedf5f256a17bafaad2899c4fd5bc9444bb44f31
Victor Ferreira <victor.ferreira@deliveryhero.com>
2019-05-31 13:31:07 +0200

    Adding multi insert transaction for Postgres

--- multiple-inserts-transaction.sql
+++ multiple-inserts-transaction.sql
@@ -1,5 +1,16 @@
+-- MYSQL
 START TRANSACTION;
 INSERT INTO table_name (name) VALUES ("some-value");
 SELECT @ID:=LAST_INSERT_ID();
 INSERT INTO other_table (external_id) VALUES (@ID);
+COMMIT;
+
+-- POSTGRES
+BEGIN;
+WITH inserted AS (
+	INSERT INTO table1 (name) VALUES ('somename') RETURNING id
+), another_insert AS (
+	INSERT INTO table2 (name, foreign_id) VALUES ('someothername', (SELECT id FROM inserted)) RETURNING id
+)
+INSERT INTO table3 (external_id) VALUES ((SELECT id FROM inserted)) RETURNING id;
 COMMIT;
